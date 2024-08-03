7288c5aea3127ed06eddf14917d18d9f4fdd2750
James-Kimanzi <jameskimanzi@gmail.com>
2024-08-03 07:50:15 +0300

    base url set to deployed backend link

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -10,7 +10,10 @@ import '../Login.css';
 import axios from 'axios';
 import { UserContext } from '../contexts/UserContext';
 
-const BASE_URL = 'http://127.0.0.1:5555';
+// const BASE_URL = 'http://127.0.0.1:5555';
+// const BASE_URL = 'http://127.0.0.1:5000';
+const BASE_URL = 'https://lost-and-found-api-81ox.onrender.com'
+
 
 const LoginForm = () => {
   const navigate = useNavigate();
