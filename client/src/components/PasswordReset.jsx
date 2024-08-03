fa9f2534991113cff2b836a8738160aaf74c0dd7
kevin <kevin@novek.io>
2024-08-03 08:57:13 +0300

    feat:updated url

--- client/src/components/PasswordReset.jsx
+++ client/src/components/PasswordReset.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-const BASE_URL = 'http://127.0.0.1:5000';
+const BASE_URL = 'https://lost-and-found-api-81ox.onrender.com';
 
 const PasswordResetRequest = () => {
   const [email, setEmail] = useState('');
